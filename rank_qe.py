8e9d3afca28c3546b684c8dbe7629172f553d97d
mingzhang96 <zmhappyer@gmail.com>
2019-07-11 09:55:14 +0800

    add rank_reranking

--- rank_qe.py
+++ rank_qe.py
@@ -49,6 +49,47 @@ def load_face(face_data):
         )
     return face_dict, movie_list
 
+def load_face_2(face_data1, face_data2):
+    face_dict = {}
+    movie_list = []
+    for movie, info in face_data1.items():
+        movie_list.append(movie)
+        casts = info['cast']
+        candidates = info['candidates']
+
+        cast_ids, cast_ffeats = [], []
+        for index, cast in enumerate(casts):
+            fea1 = cast['ffeat']
+            fea2 = face_data2[movie]['cast'][index]['ffeat']
+            assert cast['id'] == face_data2[movie]['cast'][index]['id']
+            feat = np.hstack((feat1, feat2))
+            cast_ffeats.append(feat)
+            cast_ids.append(cast['id'])
+        cast_ffeats = np.array(cast_ffeats)
+
+        candi_f_ids, candi_f_ffeats = [], []
+        for index, candidate in enumerate(candidates):
+            if candidate['ffeat'] is not None:
+                fea1 = candidate['ffeat']
+                fea2 = face_data2[movie]['candidates'][index]['ffeat']
+                assert candidate['id'] == face_data2[movie]['candidates'][index]['id']
+                feat = np.hstack((feat1, feat2))
+                candi_f_ids.append(candidate['id'])
+                candi_f_ffeats.append(ffeat)
+        candi_f_ffeats = np.array(candi_f_ffeats)
+
+        face_dict.update(
+            {
+                movie:{
+                    'cast_ids':cast_ids,
+                    'cast_ffeats':cast_ffeats,
+                    'candi_f_ids': candi_f_ids,
+                    'candi_f_ffeats':candi_f_ffeats,
+                }
+            }
+        )
+    return face_dict, movie_list
+
 def load_reid(reid_data):
     reid_dict_tmp = {}
     reid_dict = {}
@@ -188,9 +229,48 @@ def rank(movie_face, movie_reid):
     candi_ids, candi_feats = movie_reid['candi_ids'], movie_reid['candi_feats']
     movie_rank = {cast_id:[] for cast_id in cast_ids}
 
+    cast_candi_fsim = np.dot(cast_ffeats, candi_f_ffeats.T)
+    candi_candi_fsim = np.dot(candi_f_ffeats, candi_f_ffeats.T)
+    # print(candi_feats.shape)
+    candi_candi_dist = pdist(candi_feats, 'euclidean')
+    candi_candi_dist = squareform(candi_candi_dist)
+    assert cast_candi_fsim.shape[0] == len(cast_ids) and cast_candi_fsim.shape[1] == len(candi_f_ids)
+
+    # get cast_candi_flag
+    row_num, col_num = cast_candi_fsim.shape
+    cast_candi_filter = np.zeros((row_num, col_num))
+    for i, candi_id in enumerate(candi_f_ids):
+        sim = cast_candi_fsim.T[i].copy()
+        max_ind = np.argsort(sim)[-1]
+        if sim[max_ind] > 0.29:
+            cast_candi_filter[max_ind, i] = 1
+            movie_rank[cast_ids[max_ind]].append(candi_id)
+    
+    cast_candi_filter, recall_num = multi_face_recall(cast_candi_filter, candi_f_ids, candi_candi_fsim)
+
+    # multi query search
+    tmp_dist = multi_search(cast_candi_filter, candi_f_ids, candi_ids, candi_candi_dist) 
+    
+    # add res
+    for i, cast_id in enumerate(cast_ids):
+        inds = np.argsort(tmp_dist[i])
+        for idx in inds:
+            if candi_ids[idx] not in movie_rank[cast_id]:
+                movie_rank[cast_id].append(candi_ids[idx])
+
+    return movie_rank, recall_num
+
+
+
+def rank_lilei(movie_face, movie_reid):
+    cast_ids, cast_ffeats = movie_face['cast_ids'], movie_face['cast_ffeats']
+    candi_f_ids, candi_f_ffeats = movie_face['candi_f_ids'], movie_face['candi_f_ffeats']
+    candi_ids, candi_feats = movie_reid['candi_ids'], movie_reid['candi_feats']
+    movie_rank = {cast_id:[] for cast_id in cast_ids}
+
     # cast_candi_fsim_r = np.dot(cast_ffeats, candi_f_ffeats.T)
     # print(cast_candi_fsim_r.shape)
-    # cast_candi_fsim_diff = ranking(candi_f_ffeats.T,cast_ffeats.T)
+    cast_candi_fsim_diff = ranking(candi_f_ffeats.T,cast_ffeats.T)
     # print(cast_candi_fsim_diff.shape)
     # cast_candi_fsim_r = np.dot(cast_ffeats, candi_f_ffeats.T)  #n cast n*512,m candi  m*512  --->n*m
     idxs =  np.argsort(-cast_candi_fsim_diff)               #sort by score,   high --- low
@@ -210,7 +290,7 @@ def rank(movie_face, movie_reid):
     for i, candi_id in enumerate(candi_f_ids):
         sim = cast_candi_fsim.T[i].copy()
         max_ind = np.argsort(sim)[-1]
-        print(max_ind,sim[max_ind])
+        # print(max_ind,sim[max_ind])
         if sim[max_ind] > 0.38:
             cast_candi_filter[max_ind, i] = 1
             movie_rank[cast_ids[max_ind]].append(candi_id)
@@ -272,7 +352,8 @@ def rank_eval(res, label):
 
 def main(args):
     if args.is_test =='0':
-        face_feat_name = 'face_em_val_model-r100-ii.pkl'
+        face_feat_name_r50 = 'face_em_val_model-r50-am-lfw.pkl'
+        face_feat_name_r100 = 'face_em_val_model-r100-ii.pkl'
         reid_feat_name_resnet101 = 'reid_em_val_resnet101.pkl'
         reid_feat_name_densenet121 = 'reid_em_val_densenet121.pkl'
         reid_feat_name_seresnet101 = 'reid_em_val_seresnet101.pkl'
@@ -285,8 +366,9 @@ def main(args):
         reid_feat_name_seresnext101 = 'reid_em_test_seresnext101.pkl'
 
     print('Load features from pkl ...')
-    face_pkl = my_unpickle(osp.join('./features', face_feat_name))
-    face_dict, movie_list = load_face(face_pkl)
+    face_pkl_r50 = my_unpickle(osp.join('./features', face_feat_name_r50))
+    face_pkl_r100 = my_unpickle(osp.join('./features', face_feat_name_r100))
+    face_dict, movie_list = load_face_2(face_pkl_r50, face_pkl_r100)
     if args.arch is None:
         reid_pkl_resnet101 = my_unpickle(osp.join('./features', reid_feat_name_resnet101))
         reid_pkl_densenet121 = my_unpickle(osp.join('./features', reid_feat_name_densenet121))
@@ -312,10 +394,10 @@ def main(args):
     for i, movie in enumerate(movie_list):
         movie_face = face_dict[movie]
         movie_reid = reid_dict[movie]
-        # movie_rank, recall_num = rank(movie_face, movie_reid)
-        # print('movie: %s, %d/%d, recall num: %d'%(movie, i+1, movie_num, recall_num))
-        movie_rank = rank_reranking(movie_face, movie_reid)
-        print('movie: %s, %d/%d'%(movie, i+1, movie_num))
+        movie_rank, recall_num = rank_lilei(movie_face, movie_reid)
+        print('movie: %s, %d/%d, recall num: %d'%(movie, i+1, movie_num, recall_num))
+        # movie_rank = rank_reranking(movie_face, movie_reid)
+        # print('movie: %s, %d/%d'%(movie, i+1, movie_num))
         rank_list.update(movie_rank)
 
     if args.is_test == '1':
