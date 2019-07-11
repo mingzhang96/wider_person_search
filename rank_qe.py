8f250f62f092ed0daecf81049219a5d7b2f0e60a
mingzhang96 <zmhappyer@gmail.com>
2019-07-11 22:32:43 +0800

    norm

--- rank_qe.py
+++ rank_qe.py
@@ -74,9 +74,9 @@ def load_face_2(face_data1, face_data2):
             feat2 = face_data2[movie]['cast'][index]['ffeat']
             assert cast['id'] == face_data2[movie]['cast'][index]['id']
             feat = np.hstack((feat1, feat2))
-            print("before", feat.shape, feat[0])
+            print("before", feat.shape, feat)
             feat = my_norm(feat)
-            print("after", feat.shape, feat[0])
+            print("after", feat.shape, feat)
             exit()
             cast_ffeats.append(feat)
             cast_ids.append(cast['id'])
